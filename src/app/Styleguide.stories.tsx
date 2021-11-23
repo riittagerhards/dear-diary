4d52a3c2043f5db1325120b1ce4947ea1d001560
riittagerhards <rgerhards@outlook.com>
2021-11-23 18:22:15 +0100

    💄 Add global styleguide

---
+++ src/app/Styleguide.stories.tsx
@@ -0,0 +1,8 @@
+import Styleguide from './Styleguide';
+
+export default {
+  component: Styleguide,
+  title: 'Styleguide',
+};
+
+export const Default = (): JSX.Element => <Styleguide />;
---
+++ src/app/Styleguide.tsx
@@ -0,0 +1,97 @@
+import '../global.css';
+import welcomePageImage from '../App/Welcome/welcomePageImage.jpg';
+
+function Styleguide(): JSX.Element {
+  return (
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          gap: '0.3rem',
+        }}
+      >
+        <div
+          style={{
+            width: '3rem',
+            height: '3rem',
+            background: 'var(--color-primary)',
+            borderRadius: '50%',
+          }}
+        />
+        <div
+          style={{
+            width: '3rem',
+            height: '3rem',
+            background: 'var(--color-secondary)',
+            borderRadius: '50%',
+          }}
+        />
+        <div
+          style={{
+            width: '3rem',
+            height: '3rem',
+            background: 'var(--color-secondary-light)',
+            borderRadius: '50%',
+          }}
+        />
+        <div
+          style={{
+            width: '3rem',
+            height: '3rem',
+            background: 'var(--color-effect)',
+            borderRadius: '50%',
+          }}
+        />
+        <div
+          style={{
+            width: '3rem',
+            height: '3rem',
+            background: 'var(--color-background)',
+            borderRadius: '50%',
+            border: 'solid var(--color-primary) 1px',
+          }}
+        />
+      </div>
+      <h1>Header</h1>
+      <p>Paragraph</p>
+      <p>Font: Quicksand</p>
+      <p
+        style={{
+          fontFamily: 'var(--font-logo)',
+          fontSize: '1.3rem',
+        }}
+      >
+        Font: Cookie
+      </p>
+      <div
+        style={{
+          display: 'flex',
+          gap: '0.5rem',
+          alignItems: 'end',
+          marginBottom: '2rem',
+        }}
+      >
+        <img
+          src={welcomePageImage}
+          style={{ width: '8rem', height: '10rem' }}
+        />
+        <p style={{ fontSize: '0.8rem' }}>welcome page image</p>
+      </div>
+      <button
+        style={{
+          backgroundColor: 'var(--color-effect)',
+          color: 'var(--color-text-effect)',
+          fontFamily: 'var(--font-text)',
+          border: 'none',
+          width: '4rem',
+          height: '1.2rem',
+          borderRadius: 'var(--border-radius-button)',
+        }}
+      >
+        button
+      </button>
+    </div>
+  );
+}
+
+export default Styleguide;
