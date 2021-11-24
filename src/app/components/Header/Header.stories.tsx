9ff87ee16a6cacef73348d3a244572c2bb60645b
riittagerhards <rgerhards@outlook.com>
2021-11-24 15:16:36 +0100

    ♻️ Restructure code

--- src/app/components/Header/Header.stories.tsx
+++ src/app/components/Header/Header.stories.tsx
@@ -5,4 +5,12 @@ export default {
   title: 'Components/Header',
 };
 
-export const Default = (): JSX.Element => <Header />;
+export const Home = (): JSX.Element => <Header headerTitle="Hello, Riitta!" />;
+
+export const Add = (): JSX.Element => (
+  <Header headerTitle="How was your day?" />
+);
+
+export const Gallery = (): JSX.Element => <Header headerTitle="Gallery" />;
+
+export const Entry = (): JSX.Element => <Header headerTitle="01.07.2021" />;
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import styles from './Header.module.css';
 import NavigationIcon from '../Navigation/NavigationIcon.svg';
 
-function Header(): JSX.Element {
+type headerTitleProps = {
+  headerTitle: string;
+};
+
+function Header({ headerTitle }: headerTitleProps): JSX.Element {
   return (
     <div className={styles.container}>
-      <h1 className={styles.headerTitle}>How was your day?</h1>
+      <h1 className={styles.headerTitle}>{headerTitle}</h1>
       <img src={NavigationIcon} className={styles.navigationIcon} />
     </div>
   );
