bb0a0d09d899b76a021505971480e2b54bb744ee
riittagerhards <rgerhards@outlook.com>
2021-11-25 09:24:00 +0100

    ✨ Add structure for navigation

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,7 +1,26 @@
 import styles from './Navigation.module.css';
 
 function Navigation(): JSX.Element {
-  return <div className={styles.container}></div>;
+  return (
+    <nav>
+      <ul className={styles.navItems}>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Add entry</a>
+        </li>
+        <li>
+          <a href="">Gallery</a>
+        </li>
+      </ul>
+      <div className={styles.burger}>
+        <div className={styles.lineOne}></div>
+        <div className={styles.lineTwo}></div>
+        <div className={styles.lineThree}></div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
