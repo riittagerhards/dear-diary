e52f8f711728abdb21401f060f442224de096ee9
riittagerhards <rgerhards@outlook.com>
2021-11-26 13:39:13 +0100

    ✨ Add handleSubmit and onChange

--- src/app/pages/WelcomePage/WelcomePage.tsx
+++ src/app/pages/WelcomePage/WelcomePage.tsx
@@ -1,21 +1,25 @@
 import WelcomePageIcon from '../../components/Welcome/WelcomePageIcon.svg';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import styles from './WelcomePage.module.css';
 import Button from '../../components/Button/Button';
 
 function WelcomePage(): JSX.Element {
   const [name, setName] = useState('');
-  console.log(setName);
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
 
   return (
     <div className={styles.container}>
       <img className={styles.icon} src={WelcomePageIcon} alt="" />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
           className={styles.textInput}
           type="text"
           placeholder="My name"
           value={name}
+          onChange={(event) => setName(event.target.value)}
         ></input>
         <Button className={''} type={'submit'} value={'enter'} />
       </form>
