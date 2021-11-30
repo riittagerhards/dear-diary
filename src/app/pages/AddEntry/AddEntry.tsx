f4224829b4794de2b3b3f22a8a55ff048d03a87e
riittagerhards <rgerhards@outlook.com>
2021-11-30 14:44:37 +0100

    🎨 Add review comments

--- src/app/pages/AddEntry/AddEntry.tsx
+++ src/app/pages/AddEntry/AddEntry.tsx
@@ -13,21 +13,17 @@ function AddEntry(): JSX.Element {
           type="text"
           placeholder="... add title ..."
           required
-        / >
+        />
         <label className={styles.imageUpload}>
           <img src={UploadImageIcon} />
-          <input
-            className={styles.imageInput}
-            type="file"
-            accept="image/*"
-          ></input>
+          <input className={styles.imageInput} type="file" accept="image/*" />
         </label>
         <input className={styles.dateInput} type="date" required></input>
         <input
           className={styles.textInput}
           type="text"
           placeholder="... add text ..."
-        ></input>
+        />
         <Button className={styles.button} type={'submit'} value={'save'} />
       </form>
     </div>
