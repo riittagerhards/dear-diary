920f40b967dfb8704188b1b50d06a23d686ed944
riittagerhards <rgerhards@outlook.com>
2021-12-03 11:33:42 +0100

    ✨ Add dynamic

--- src/app/components/GalleryCard/GalleryCard.stories.tsx
+++ src/app/components/GalleryCard/GalleryCard.stories.tsx
@@ -5,4 +5,10 @@ export default {
   title: 'Components/GalleryCard',
 };
 
-export const Default = () => <GalleryCard />;
+export const Default = () => (
+  <GalleryCard
+    day="03"
+    month="DEZ"
+    src="https://images.pexels.com/photos/64227/frosted-leaf-branch-frost-64227.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+  />
+);
--- src/app/components/GalleryCard/GalleryCard.tsx
+++ src/app/components/GalleryCard/GalleryCard.tsx
@@ -1,17 +1,19 @@
 import styles from './GalleryCard.module.css';
 
-function GalleryCard(): JSX.Element {
+type GalleryCardProps = {
+  day: string;
+  month: string;
+  src: string;
+};
+
+function GalleryCard({ day, month, src }: GalleryCardProps): JSX.Element {
   return (
     <div className={styles.container}>
       <span className={styles.dateContainer}>
-        <p className={styles.dateDay}>03</p>
-        <p className={styles.dateMonth}>DEZ</p>
+        <p className={styles.dateDay}>{day}</p>
+        <p className={styles.dateMonth}>{month}</p>
       </span>
-      <img
-        className={styles.image}
-        src="https://images.pexels.com/photos/64227/frosted-leaf-branch-frost-64227.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
-        alt="my picture of the day"
-      />
+      <img className={styles.image} src={src} alt="my picture of the day" />
     </div>
   );
 }
