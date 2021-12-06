677407f52aeb68420e762c7842b953a91a07acfd
riittagerhards <rgerhards@outlook.com>
2021-12-06 15:37:56 +0100

    ✨ Get all entries in select element

--- src/app/pages/Gallery/Gallery.tsx
+++ src/app/pages/Gallery/Gallery.tsx
@@ -2,15 +2,30 @@ import Navigation from '../../components/Navigation/Navigation';
 import GalleryCard from '../../components/GalleryCard/GalleryCard';
 import useGetEntries from '../../utils/useGetEntries';
 import styles from './Gallery.module.css';
+import { useState } from 'react';
 
 function Gallery(): JSX.Element {
   const entries = useGetEntries();
+  const [selectDate, setSelectDate] = useState([]);
+
+  const handleChange = (event) => {
+    setSelectDate(event.target.value);
+  };
+
+  const dateOptions = entries?.map((entry) => (
+    <option key={entry.date}>{entry.date}</option>
+  ));
 
   return (
     <div className={styles.container}>
       <Navigation headerTitle={'Gallery'} />
-      <select className={styles.select}>
+      <select
+        className={styles.select}
+        onChange={handleChange}
+        value={selectDate}
+      >
         <option>Select Month</option>
+        {dateOptions}
       </select>
       <div>
         {entries?.length === 0 && (
