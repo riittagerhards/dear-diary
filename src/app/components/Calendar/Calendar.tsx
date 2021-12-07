dee41bfbdd416d63955a382c9e48441e628d0ba5
riittagerhards <rgerhards@outlook.com>
2021-12-07 17:10:43 +0100

    🚧 Start setting selected dates

--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -1,12 +1,29 @@
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import { Calendar } from 'react-modern-calendar-datepicker';
 import './Calendar.styles.css';
+import useGetEntries from '../../utils/useGetEntries';
+import { useState } from 'react';
 
 type CalendarProps = {
   className: string;
 };
 
 function CalendarSummary({ className }: CalendarProps): JSX.Element {
+  const [entryDays, setEntryDays] = useState([]);
+
+  const entries = useGetEntries();
+
+  const selectedDates = entries?.map((entry) => {
+    const date = new Date(entry.date);
+    return {
+      year: date.getFullYear(),
+      month: date.getMonth(),
+      day: date.getDate(),
+    };
+  });
+
+  console.log(selectedDates);
+
   const minimumDate = {
     year: 2021,
     month: 1,
@@ -19,6 +36,7 @@ function CalendarSummary({ className }: CalendarProps): JSX.Element {
         calendarClassName="calendar"
         calendarTodayClassName="today"
         minimumDate={minimumDate}
+        value={entryDays}
       />
     </div>
   );
