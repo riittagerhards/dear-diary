503e13c421cac6f73adfb8dce4ecbfb958ea4b6c
riittagerhards <rgerhards@outlook.com>
2021-12-07 19:08:05 +0100

    ✨ Highlight days with diary entries

--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -2,27 +2,23 @@ import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import { Calendar } from 'react-modern-calendar-datepicker';
 import './Calendar.styles.css';
 import useGetEntries from '../../utils/useGetEntries';
-import { useState } from 'react';
 
 type CalendarProps = {
   className: string;
 };
 
 function CalendarSummary({ className }: CalendarProps): JSX.Element {
-  const [entryDays, setEntryDays] = useState([]);
-
   const entries = useGetEntries();
 
   const selectedDates = entries?.map((entry) => {
     const date = new Date(entry.date);
     return {
       year: date.getFullYear(),
-      month: date.getMonth(),
+      month: date.getMonth() + 1,
       day: date.getDate(),
+      className: 'highlightedDays',
     };
   });
-
-  console.log(selectedDates);
 
   const minimumDate = {
     year: 2021,
@@ -36,7 +32,7 @@ function CalendarSummary({ className }: CalendarProps): JSX.Element {
         calendarClassName="calendar"
         calendarTodayClassName="today"
         minimumDate={minimumDate}
-        value={entryDays}
+        customDaysClassName={selectedDates}
       />
     </div>
   );
