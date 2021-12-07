f68e844d420853238510744427eba11222ac77b7
riittagerhards <rgerhards@outlook.com>
2021-12-07 14:50:06 +0100

    ✨ Add home page with current date and import calender

--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -2,7 +2,11 @@ import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import { Calendar } from 'react-modern-calendar-datepicker';
 import './Calendar.styles.css';
 
-function CalendarSummary(): JSX.Element {
+type CalendarProps = {
+  className: string;
+};
+
+function CalendarSummary({ className }: CalendarProps): JSX.Element {
   const minimumDate = {
     year: 2021,
     month: 1,
@@ -10,11 +14,13 @@ function CalendarSummary(): JSX.Element {
   };
 
   return (
-    <Calendar
-      calendarClassName="calendar"
-      calendarTodayClassName="today"
-      minimumDate={minimumDate}
-    />
+    <div className={className}>
+      <Calendar
+        calendarClassName="calendar"
+        calendarTodayClassName="today"
+        minimumDate={minimumDate}
+      />
+    </div>
   );
 }
 
