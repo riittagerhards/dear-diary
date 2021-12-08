52b2673f855b4632a73ced12021779b63c766757
riittagerhards <rgerhards@outlook.com>
2021-12-08 11:51:30 +0100

    💄 Finish styling

--- src/app/pages/SingleEntry/SingleEntry.tsx
+++ src/app/pages/SingleEntry/SingleEntry.tsx
@@ -12,11 +12,12 @@ function SingleEntry(): JSX.Element {
           src="https://images.pexels.com/photos/64227/frosted-leaf-branch-frost-64227.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
           alt="my picture of the day"
         />
-        <h1>Title</h1>
+        <h1>Lorem ipsum dolor sit amet</h1>
         <p className={styles.text}>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam volu.
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea ta.
         </p>
         <label className={styles.buttonContainer}>
           <Button type={'button'} name={'edit'} />
