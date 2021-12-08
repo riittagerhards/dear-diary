b4a2565ce00a4c17de175a33a76cc51b0854190c
riittagerhards <rgerhards@outlook.com>
2021-12-08 12:51:17 +0100

    🎨 Make the page dynamic

--- src/app/pages/SingleEntry/SingleEntry.tsx
+++ src/app/pages/SingleEntry/SingleEntry.tsx
@@ -2,23 +2,26 @@ import styles from './SingleEntry.module.css';
 import Navigation from '../../components/Navigation/Navigation';
 import Button from '../../components/Button/Button';
 
-function SingleEntry(): JSX.Element {
+type SingleEntryProps = {
+  headerTitle: string;
+  src: string;
+  entryTitle: string;
+  text: string;
+};
+
+function SingleEntry({
+  headerTitle,
+  src,
+  entryTitle,
+  text,
+}: SingleEntryProps): JSX.Element {
   return (
     <div>
-      <Navigation headerTitle={'December 8, 2021'} />
+      <Navigation headerTitle={headerTitle} />
       <div className={styles.container}>
-        <img
-          className={styles.image}
-          src="https://images.pexels.com/photos/64227/frosted-leaf-branch-frost-64227.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
-          alt="my picture of the day"
-        />
-        <h1>Lorem ipsum dolor sit amet</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea ta.
-        </p>
+        <img className={styles.image} src={src} alt="my picture of the day" />
+        <h1>{entryTitle}</h1>
+        <p className={styles.text}>{text}</p>
         <label className={styles.buttonContainer}>
           <Button type={'button'} name={'edit'} />
           <Button type={'submit'} name={'delete'} />
