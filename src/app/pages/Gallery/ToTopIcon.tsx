f806167dc89a0737a3c694698f05bb6045300c6f
riittagerhards <rgerhards@outlook.com>
2021-12-16 18:21:33 +0100

    💄 Adjust to top arrow

--- src/app/pages/Gallery/ToTopIcon.tsx
+++ src/app/pages/Gallery/ToTopIcon.tsx
@@ -8,14 +8,14 @@ function ToTopIcon({ className, onClick }: ToTopIconProps): JSX.Element {
     <svg
       className={className}
       onClick={onClick}
-      width="25"
-      height="25"
-      viewBox="0 0 25 25"
+      width="24"
+      height="30"
+      viewBox="0 0 24 30"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M12.5 0L25 12.931H19.1667V25H5.83333V12.931H0L12.5 0Z"
+        d="M13.0607 0.93934C12.4749 0.353553 11.5251 0.353553 10.9393 0.93934L1.3934 10.4853C0.807612 11.0711 0.807612 12.0208 1.3934 12.6066C1.97918 13.1924 2.92893 13.1924 3.51472 12.6066L12 4.12132L20.4853 12.6066C21.0711 13.1924 22.0208 13.1924 22.6066 12.6066C23.1924 12.0208 23.1924 11.0711 22.6066 10.4853L13.0607 0.93934ZM13.5 30L13.5 2H10.5L10.5 30H13.5Z"
         fill="white"
       />
     </svg>
