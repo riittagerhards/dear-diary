db6579e1e6bff06370f7afb09fd4fa9e46ca5b37
riittagerhards <rgerhards@outlook.com>
2021-12-26 16:17:57 +0100

    Add show text when no image in gallery

--- src/app/components/GalleryCard/GalleryCard.stories.tsx
+++ src/app/components/GalleryCard/GalleryCard.stories.tsx
@@ -5,9 +5,16 @@ export default {
   title: 'Components/GalleryCard',
 };
 
-export const Default = () => (
+export const Image = () => (
   <GalleryCard
     date={new Date(2012 - 12 - 0o3)}
     src="https://images.pexels.com/photos/64227/frosted-leaf-branch-frost-64227.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+  />
+);
+
+export const Text = () => (
+  <GalleryCard
+    date={new Date(2012 - 12 - 0o3)}
+    text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea ta."
   />
 );
