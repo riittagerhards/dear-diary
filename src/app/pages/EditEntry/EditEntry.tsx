b6ef7e6507021610f8637d6c90bd742050b58a19
riittagerhards <rgerhards@outlook.com>
2021-12-29 16:15:08 +0100

    ✨ Add en empty page if nothing to edit

--- src/app/pages/EditEntry/EditEntry.tsx
+++ src/app/pages/EditEntry/EditEntry.tsx
@@ -3,8 +3,9 @@ import Button from '../../components/Button/Button';
 import Navigation from '../../components/Navigation/Navigation';
 import UploadImage from '../../components/UploadImage/UploadImage';
 import useEditEntry from '../../utils/useEditEntry';
+import EmptyIcon from '../SingleEntry/EmptyIcon';
 import { FormEvent, useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 type SingleEntryProps = {
   imageUrl: string;
@@ -56,35 +57,54 @@ function EditEntry(): JSX.Element {
 
   return (
     <div className={styles.container}>
-      <Navigation headerTitle={'Make some changes!'} />
-      <form className={styles.form} onSubmit={handleSubmit}>
-        {imageUrl ? (
-          <img src={imageUrl} className={styles.image} />
-        ) : (
-          <span className={styles.imageContainer}>
-            <img src={entry?.imageUrl} className={styles.image} />
-            <UploadImage onUpload={setImageUrl} />
-          </span>
-        )}
-        <input
-          className={styles.titleInput}
-          type="text"
-          placeholder={entry?.title}
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-          required
-        />
-        <input className={styles.dateInput} type="date" value={date} readOnly />
-        <textarea
-          className={styles.textInput}
-          maxLength={250}
-          rows={7}
-          value={text}
-          placeholder={entry?.text}
-          onChange={(event) => setText(event.target.value)}
-        />
-        <Button className={styles.button} type={'submit'} name={'Save'} />
-      </form>
+      {entry ? (
+        <Navigation headerTitle={'Make some changes!'} />
+      ) : (
+        <Navigation headerTitle={'Blank space'} />
+      )}
+      {entry ? (
+        <form className={styles.form} onSubmit={handleSubmit}>
+          {imageUrl ? (
+            <img src={imageUrl} className={styles.image} />
+          ) : (
+            <span className={styles.imageContainer}>
+              <img src={entry?.imageUrl} className={styles.image} />
+              <UploadImage onUpload={setImageUrl} />
+            </span>
+          )}
+          <input
+            className={styles.titleInput}
+            type="text"
+            placeholder={entry?.title}
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            required
+          />
+          <input
+            className={styles.dateInput}
+            type="date"
+            value={date}
+            readOnly
+          />
+          <textarea
+            className={styles.textInput}
+            maxLength={250}
+            rows={7}
+            value={text}
+            placeholder={entry?.text}
+            onChange={(event) => setText(event.target.value)}
+          />
+          <Button className={styles.button} type={'submit'} name={'Save'} />
+        </form>
+      ) : (
+        <article className={styles.empty}>
+          <EmptyIcon />
+          Sorry, nothing to edit here!
+          <Link to={'/add'} className={styles.link}>
+            Do you want to add something?
+          </Link>
+        </article>
+      )}
     </div>
   );
 }
