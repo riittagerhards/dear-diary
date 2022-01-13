429067008cd8201cb6611d614a1ccae9418a1b59
riittagerhards <rgerhards@outlook.com>
2022-01-13 16:44:21 +0100

    🚚 Rename files

---
+++ src/app/components/PopUp/Popup.stories.tsx
@@ -0,0 +1,13 @@
+import Popup from './PopUp';
+
+export default {
+  component: Popup,
+  title: 'Components/Popup',
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const Default = (): JSX.Element => (
+  <Popup message={'You cannot add a day twice'} />
+);
